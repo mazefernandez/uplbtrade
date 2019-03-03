8b7209fb483b4152bc440e934abe69161156942f
mazefernandez <aafernandez5@up.edu.ph>
2019-03-03 15:07:27 +0800

    Fix Profile Activity, remove sign out button

--- app/src/main/java/com/mazefernandez/uplbtrade/activities/ProfileActivity.java
+++ app/src/main/java/com/mazefernandez/uplbtrade/activities/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -66,7 +67,6 @@ public class ProfileActivity extends AppCompatActivity {
         editCustomer = findViewById(R.id.editCustomer);
         addItem = findViewById(R.id.addItem);
         recyclerView = findViewById(R.id.recycler_view);
-        Button signOut = findViewById(R.id.sign_out);
 
         /* Configure Google Sign in */
         final GoogleSignInClient googleSIC = googleAdapter.configureGoogleSIC(this);
@@ -127,32 +127,15 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
-        /* Sign-out implementation */
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                googleSIC.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        //brings user to login after sign out
-                        Intent intent = new Intent(ProfileActivity.this,LoginActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                    }
-                });
-            }
-        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
 
-        /* Accepts data from edit activity */
         if (resultCode == Activity.RESULT_OK)
             switch (requestCode) {
+                /* Results from edit activity */
                 case 2:
                     Bundle editInfo = data.getExtras();
                     assert editInfo != null;
@@ -164,6 +147,7 @@ public class ProfileActivity extends AppCompatActivity {
                     profileAddress.setText(editAddress);
                     contactNo.setText(editContactNo);
                     break;
+                /* Results from add item */
                 case 3:
                     if (getIntent().hasExtra("NAME")) {
                         Bundle itemInfo = data.getExtras();
@@ -195,4 +179,5 @@ public class ProfileActivity extends AppCompatActivity {
         itemArrayList.add(new Item(2,"CASIO Scientific Calculator","",400.00, null,"Old",1));
         itemArrayList.add(new Item(3,"Hum 3 Reader","",150.00, null,"Never used",1));
     }
+
 }
