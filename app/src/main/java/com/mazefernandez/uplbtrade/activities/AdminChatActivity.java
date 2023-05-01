2bd1fa1bcff408428deaedda65d6cb338924a1aa
mazefernandez <aafernandez5@up.edu.ph>
2023-05-01 19:34:33 +0800

    add admin chat

---
+++ app/src/main/java/com/mazefernandez/uplbtrade/activities/AdminChatActivity.java
@@ -0,0 +1,16 @@
+package com.mazefernandez.uplbtrade.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mazefernandez.uplbtrade.R;
+
+public class AdminChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_chat);
+    }
+}
